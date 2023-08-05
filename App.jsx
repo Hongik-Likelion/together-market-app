9ba86b08e4902f24af9325ef7da69742e0fc7317
jisooshin <kelly6226@naver.com>
2023-08-05 23:52:32 +0900

    [FEAT] 01-로그인 화면1 레이아웃 디자인

--- App.jsx
+++ App.jsx
@@ -1,23 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '@screens/LoginScreen';
+import CommonSignUpScreen from '@screens/CommonSignUpScreen';
 
+const Stack = createNativeStackNavigator();
+
+//라우팅관련된 것은 다 app.js에서 (최상위 라우팅은 여기서)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      <Text>fdjfkdj</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={'login'}
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name={'commonSignUp'}
+          component={CommonSignUpScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/CommonSignUpScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { styled } from 'styled-components/native';
+
+function CommonSignUpScreen() {
+  return <Container>figma상 03-회원가입(기본) 페이지 생성 예정</Container>;
+}
+
+const Container = styled.Text`
+  margin-top: 200px;
+  margin-left: 70px;
+`;
+
+export default CommonSignUpScreen;
