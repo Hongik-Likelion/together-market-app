f7f6dd1254378e13869f7a01ca6ed9b7a6be59f8
yongun2 <djkye0001@naver.com>
2023-08-05 20:47:51 +0900

    [DOC] 라이브러리 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+          alias: {
+            '~': '.',
+            '@api': './api',
+            '@components': './components',
+            '@hooks': './hooks',
+            '@screens': './screens',
+            '@assets': './assets',
+          },
+        },
+      ],
+    ],
   };
 };
