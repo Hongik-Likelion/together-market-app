6803951abebf83ab76bbbe649f25830f8c7e984e
yongun2 <djkye0001@naver.com>
2023-08-07 21:05:02 +0900

    [FEAT] navigation/bottomTab 적용으로 인한 컴포넌트 일부 주석 처리

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,25 +1,23 @@
-import Category from '@components/Category';
-import Header from '@components/Header';
-import Post from '@components/Post';
-import Tab from '@components/Tab';
 import React from 'react';
 import { styled } from 'styled-components/native';
-
+import Category from '@components/home/Category';
+import Post from '@components/home/Post';
+import Header from '@components/home/Header';
 
 function HomeScreen() {
-    return (
-        <Container>
-            <Header></Header>
-            <Category></Category>
-            <Post></Post>
-            <Tab></Tab>
-        </Container>
-    );
+  return (
+    <Container>
+      {/* <Header></Header> */}
+      <Category></Category>
+      <Post></Post>
+      {/* <Tab></Tab> */}
+    </Container>
+  );
 }
 
 const Container = styled.View`
-    flex: 1;
-    background-color: #ffffff;
-`
+  flex: 1;
+  background-color: #ffffff;
+`;
 
 export default HomeScreen;
