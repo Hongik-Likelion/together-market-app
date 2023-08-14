189bb6be02b06fc9f1cf6e439c23e1502edbf962
yongun2 <djkye0001@naver.com>
2023-08-14 15:36:17 +0900

    [FIX] home 라우팅 이름 겹치는 이슈 해결

--- routes/home/HomeScreenNavigator.jsx
+++ routes/home/HomeScreenNavigator.jsx
@@ -6,21 +6,14 @@ import HomeDetailScreen from '@screens/HomeDetailScreen';
 const Stack = createNativeStackNavigator();
 
 function HomeScreenNavigator() {
-    return (
-        <Stack.Navigator initialRouteName={'home'} screenOptions={{ headerShown: false }}>
-            {/*기본적으로 홈탭 눌렀을 때*/}
-            <Stack.Screen
-                name={'home'}
-                component={HomeScreen}
-            />
-            {/*게시물 눌렀을 떄 더 자세하게 게시물 보여줌*/}
-            <Stack.Screen
-                name={'home-detail'}
-                component={HomeDetailScreen}
-                
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName={'home'} screenOptions={{ headerShown: false }}>
+      {/*기본적으로 홈탭 눌렀을 때*/}
+      <Stack.Screen name={'home-list'} component={HomeScreen} />
+      {/*게시물 눌렀을 떄 더 자세하게 게시물 보여줌*/}
+      <Stack.Screen name={'home-detail'} component={HomeDetailScreen} />
+    </Stack.Navigator>
+  );
 }
 
 export default HomeScreenNavigator;
