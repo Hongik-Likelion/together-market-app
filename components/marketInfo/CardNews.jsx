2e496e74343a492c9a3a5b1b29a7aa828c6e9696
yongun2 <djkye0001@naver.com>
2023-08-17 16:06:57 +0900

    [FIX] px 누락

--- components/marketInfo/CardNews.jsx
+++ components/marketInfo/CardNews.jsx
@@ -5,41 +5,37 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { useNavigation } from '@react-navigation/native';
 
-function CardNews({cardData, onPressCardNewsDetail}) {
-    return (
-        <Container onPress={onPressCardNewsDetail}>
-            <CardImg>
-                {cardData.cardImg}
-            </CardImg>
-            <CardContent numberOfLines={2}>
-                {cardData.cardCategory}{cardData.cardContent1}
-            </CardContent>
-        </Container>
-    );
+function CardNews({ cardData, onPressCardNewsDetail }) {
+  return (
+    <Container onPress={onPressCardNewsDetail}>
+      <CardImg>{cardData.cardImg}</CardImg>
+      <CardContent numberOfLines={2}>
+        {cardData.cardCategory}
+        {cardData.cardContent1}
+      </CardContent>
+    </Container>
+  );
 }
 
 const Container = styled.TouchableOpacity`
+  padding-left: ${wp(4)}px;
+  padding-right: ${wp(4)}px;
 
-    padding-left: ${wp(4)}px;
-    padding-right: ${wp(4)}px;
+  width: ${wp(50)}px;
 
-    width: ${wp(50)}px;
-
-    margin-top: ${hp(2.5)}px; 
-    flex: 0 0 50%; /* flex-grow, flex-shrink, flex-basis*/
+  margin-top: ${hp(2.5)}px;
+  flex: 0 0 50%; /* flex-grow, flex-shrink, flex-basis*/
 `;
 
 const CardImg = styled.View`
-    align-items: center;
-
+  align-items: center;
 `;
 
 const CardContent = styled.Text`
-    font-size: ${RFValue(13)};
-    font-weight: 700;
-    padding-top: ${hp(1.5)}px;
-    line-height: 25px;
+  font-size: ${RFValue(13)}px;
+  font-weight: 700;
+  padding-top: ${hp(1.5)}px;
+  line-height: 25px;
 `;
-
 
 export default CardNews;
