433b06e25008d24dd5f8998a0a083e2ce919cf68
yongun2 <djkye0001@naver.com>
2023-08-18 04:02:26 +0900

    [FEAT] 로그인 기능 서버와 연동

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,8 @@
 import { client } from './client';
 
 // ex
-// const fetchUserInfo = client.get("/user/info");
+const fetchUserInfo = client.get('/user/info');
 
-// export {fetchUserInfo};
+const login = (email) => client.post('/user/login/', { email });
+
+export { fetchUserInfo, login };
