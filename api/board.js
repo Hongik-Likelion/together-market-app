46f5f1f751883658439bfd4fb8424340a672ca97
jisooshin <kelly6226@naver.com>
2023-12-20 02:01:28 +0900

    [FEAT] 홈화면 게시물 띄우기 API 연동

--- api/board.js
+++ api/board.js
@@ -1,23 +1,29 @@
-import { client } from "./client";
+import { client } from './client';
 
 // 게시글 리스트 조회(시장에 따른)
-const fetchBoardList = () => client.get('/board', {
-    params: {market_id},
-    headers:{
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ"
-    }
-    
-});
+const fetchBoardList = (marketId) =>
+  client.get('/board', {
+    params: { market_id: marketId },
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ',
+    },
+  });
 
-const doLike = (board_id) => client.patch(`/board/${board_id}/like/`,{
-    headers:{
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ"
-    }
-})
-const doUnlike = (board_id) => client.patch(`/board/${board_id}/unlike/`,{
-    headers:{
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ"
-    }
-})
+const doLike = (data) =>
+  client.patch(`/board/${data}/like/`, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ',
+    },
+  });
 
-export {fetchBoardList, doLike, doUnlike}
+const doUnlike = (board_id) =>
+  client.patch(`/board/${board_id}/unlike/`, {
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhQGEuY29tIiwiaWQiOjQsImV4cCI6MTcwMzU5ODU2MX0.4SbyqXdyMp9ZvKv4A4Qq0luq1YufLKLRWTz_CynGsxQ',
+    },
+  });
+
+export { fetchBoardList, doLike, doUnlike };
--- components/home/Post.jsx
+++ components/home/Post.jsx
@@ -1,35 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components/native';
 import PostItem from './PostItem';
 import { FlatList } from 'react-native';
-import axios from 'axios';
-import { useFetchData } from '@hooks/fetch';
 import { fetchBoardList } from 'api/board';
 import format from 'pretty-format';
+import { View, Text } from 'react-native';
 
 function Post() {
+  //const [isLoading, isError, posts] = useFetchData(fetchBoardList);
 
-  const [isLoading, isError, posts] = useFetchData(fetchBoardList);
+  // 게시글 리스트 조회 API
+  const [postsData, setPostsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  
-  if (isLoading) return null;
+  // !!!!!!!!!! 이거 추후 API에서 return 받은걸로 변경 필요.
+  const market_id = 1;
 
-  if (isError) return null;
+  useEffect(() => {
+    setIsLoading(true);
+    fetchBoardList(market_id)
+      .then((res) => {
+        console.log(format(res.data));
+        setPostsData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log('error fetchBoardList');
+        console.log(err);
+        setIsError(true);
+        setIsLoading(false);
+      });
+  }, []);
 
-  if (posts!==null) {
-    console.log('통신 성공:', posts)
+  if (isLoading) {
     return (
-      <Container>
-        <FlatList
-          data={posts}
-          renderItem={({item}) => <PostItem post={item}/>}
-        
-        />
-      </Container>
+      <View>
+        <Text>로딩중...</Text>
+      </View>
     );
-  } else {
-    console.log('통신실패')
   }
+
+  if (isError) {
+    return (
+      <View>
+        <Text>에러 발생</Text>
+      </View>
+    );
+  }
+
+  return (
+    <Container>
+      <FlatList data={postsData} renderItem={({ item }) => <PostItem post={item} />} />
+    </Container>
+  );
 }
 
 const Container = styled.View`
