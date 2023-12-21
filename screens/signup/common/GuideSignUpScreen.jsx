627a17bc88e20fa9721fbcb6a621adae3ef2e3bf
yongun2 <djkye0001@naver.com>
2023-12-21 10:23:48 +0900

    merge develop

--- screens/signup/common/GuideSignUpScreen.jsx
+++ screens/signup/common/GuideSignUpScreen.jsx
@@ -1,20 +1,84 @@
-import { PreviousBtn, ContinueBtn } from '@assets/signUp/CommonSignUpScreenIcon';
+import { ContinueBtn, PreviousBtn } from '@assets/signUp/CommonSignUpScreenIcon';
 import GuideOption from '@components/signUp/guide/GuideOption';
 import GuideTopTab from '@components/signUp/guide/GuideTopTab';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
+import { postfavMarket, signUp } from 'api/auth';
 import { COLORS } from 'colors';
-import React, { useContext, useState, useEffect } from 'react';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { styled } from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import { Auth } from 'context/AuthContext';
 import { UserInfo } from 'context/UserInfoContext';
-import { View, Text } from 'react-native';
+import format from 'pretty-format';
+import React, { useContext, useState } from 'react';
+import { Alert, Text } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { styled } from 'styled-components/native';
 
 function GuideSignUpScreen(props) {
+  const {
+    user: [signUpRequest, setSignUpRequest],
+    market: [favouriteMarketRequest, setFavouriteMarketRequest],
+  } = useContext(Auth);
+
   const navigation = useNavigation();
   const { userType } = useContext(UserInfo);
 
   const [guide, selectGuide] = useState(0); //guide가 1이면 보겠다, 2면 안보겠다
+
+  const onPressContinueBtn = async () => {
+    // 회원가입과 자주가는 시장 등록
+    try {
+      await handleSignUp();
+      if (userType === 1) {
+      } else await handleFavoriteMarket();
+      navigation.navigate('guideSignUpScreen'); // 이동은 여기서 호출
+
+      if (guide === 1) {
+        navigation.reset({
+          routes: [
+            {
+              name: 'home-tab',
+              state: {
+                routes: [
+                  {
+                    name: 'marketInfo',
+                  },
+                ],
+              },
+            },
+          ],
+        });
+      } else {
+        navigation.reset({ routes: [{ name: 'home-tab' }] });
+      }
+    } catch (err) {
+      console.log(format(err));
+      return;
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const response = await signUp(signUpRequest);
+      const { accessToken, refreshToken } = response.data;
+
+      await AsyncStorage.setItem('access_token', accessToken);
+      await AsyncStorage.setItem('refresh_token', refreshToken);
+    } catch (signUpErr) {
+      Alert.alert('회원가입 오류', '잠시 후 다시 진행해 주세요');
+      throw signUpErr;
+    }
+  };
+
+  const handleFavoriteMarket = async () => {
+    try {
+      await postfavMarket(favouriteMarketRequest);
+    } catch (addFavoriteMarketErr) {
+      console.log(format(addFavoriteMarketErr));
+      Alert.alert('자주가는 시장 등록 오류', '잠시 후 다시 진행해 주세요');
+      throw addFavoriteMarketErr;
+    }
+  };
 
   const onPressPreviousBtn = () => {
     selectGuide('');
@@ -22,14 +86,6 @@ function GuideSignUpScreen(props) {
       navigation.navigate('ownerSignUpSpecificScreen');
     } else if (userType === 2) {
       navigation.navigate('userSignUpScreen');
-    }
-  };
-
-  const onPressContinueBtn = () => {
-    if (guide === 1) {
-      navigation.navigate('home-tab', { screen: 'marketInfo' });
-    } else {
-      navigation.navigate('home-tab');
     }
   };
 
